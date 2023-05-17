ae66902a8f49f1a281a7d5aad3475cd5b171813a
XLG123 <xiaolin.guan123@gmail.com>
2023-05-17 14:35:13 -0400

    finished fibonacci numbers

--- v.rb
+++ v.rb
@@ -1,30 +1,29 @@
-  def range(start,end_num)
-    return []  if end_num <start 
-    arr=[start]
-    arr+range(start+1,end_num)- [end_num]
+def range(start,end_num)
+  return [] if end_num <start 
+  arr = [start]
+  arr + range(start + 1,end_num) - [end_num]
 
+end
+
+def itera_sum(arr)
+  sum = 0
+  arr.each do |num|
+    sum += num
   end
+  sum
+end
 
-  def itera_sum(arr)
-    sum = 0
-    arr.each do |num|
-      sum += num
-    end
-    sum
-  end
+def recur_sum(arr)
+  return [] if arr.empty?
+  return arr[0] if arr.length == 1
+  arr[0] + recur_sum(arr[1..-1])
+end
 
-  def recur_sum(arr)
-    return [] if arr.empty?
-    return arr[0] if arr.length == 1
-    arr[0] + recur_sum(arr[1..-1])
-  end
-
-  # p itera_sum([1, 2, 3])
-  # p recur_sum([1, 2, 3])
-  # p recur_sum([])
+# p itera_sum([1, 2, 3])
+# p recur_sum([1, 2, 3])
+# p recur_sum([])
 
 # p  range(1,5)
-
 
 # first version
 # def exp(b, n)
@@ -34,19 +33,16 @@
 # p  exp(2, 0)
 #  p  exp(2, 3)
 
-
-
 # 2nd version
-
 def exp(b, n)
-  return 1  if n==0 
-  return b if n==1
+  return 1  if n == 0 
+  return b if n == 1
   if n.even?
     exp(b, n / 2) ** 2  
   else
     b * (exp(b, (n - 1) / 2) ** 2)
   end
- end
+end
 
 #  p  exp(2, 0)
 #  p  exp(2, 1)
@@ -55,27 +51,31 @@ def exp(b, n)
 #  p  exp(2, 4)
 
 
-  def deep_dup(arr,new_arr=[])
-    return [] if arr.empty?
+def deep_dup(arr,new_arr=[])
+  return [] if arr.empty?
 
-    arr.each do |ele|
-      if ele.is_a?Array
-        deep_dup(ele,new_arr)
-      else
-        new_arr<<ele
+  arr.each do |ele|
+    if ele.is_a?Array
+      deep_dup(ele,new_arr)
+    else
+      new_arr<<ele
     end
-    end
-    new_arr
   end
+
+  new_arr
+end
 # arr1=[1,2,3]
 # arr4=deep_dup(arr1)
+
 # p arr1.object_id
 # p arr4.object_id
+
 # p arr4
-#  arr4[1]=8
+# arr4[1]=8
 # p arr4
 # p arr1
 # p arr1
+
 # arr2=[1,2,[3]]
 # p deep_dup(arr2)
 # arr3=deep_dup(arr2)
@@ -86,3 +86,13 @@ def exp(b, n)
 
 # arr3=[]
 # p deep_dup(arr3)
+
+def fib(n)
+  return [0] if n == 1
+  return [0, 1] if n == 2
+  arr = fib(n-1)
+  arr << arr[-1] + arr[-2]
+end
+
+p fib(4)
+p fib(10)
