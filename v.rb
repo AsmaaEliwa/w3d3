446fc0b3af84e5d8233d5dabef591c3e0caed2cd
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-05-17 14:11:47 -0400

    deep_dup completed

--- v.rb
+++ v.rb
@@ -26,11 +26,11 @@
 # p  range(1,5)
 
 
-first version
-def exp(b, n)
- return 1  if n==0 
- b * exp(b, n - 1)
-end
+# first version
+# def exp(b, n)
+#  return 1  if n==0 
+#  b * exp(b, n - 1)
+# end
 # p  exp(2, 0)
 #  p  exp(2, 3)
 
@@ -48,8 +48,41 @@ def exp(b, n)
   end
  end
 
- p  exp(2, 0)
- p  exp(2, 1)
+#  p  exp(2, 0)
+#  p  exp(2, 1)
 
- p  exp(2, 3)
- p  exp(2, 4)
+#  p  exp(2, 3)
+#  p  exp(2, 4)
+
+
+  def deep_dup(arr,new_arr=[])
+    return [] if arr.empty?
+
+    arr.each do |ele|
+      if ele.is_a?Array
+        deep_dup(ele,new_arr)
+      else
+        new_arr<<ele
+    end
+    end
+    new_arr
+  end
+# arr1=[1,2,3]
+# arr4=deep_dup(arr1)
+# p arr1.object_id
+# p arr4.object_id
+# p arr4
+#  arr4[1]=8
+# p arr4
+# p arr1
+# p arr1
+# arr2=[1,2,[3]]
+# p deep_dup(arr2)
+# arr3=deep_dup(arr2)
+# p arr3
+# arr3[2]=[0]
+# p arr3
+# p arr2
+
+# arr3=[]
+# p deep_dup(arr3)
