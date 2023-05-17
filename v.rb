923091245d0b31ff0885f72a72ff4cf4312b4e8f
XLG123 <xiaolin.guan123@gmail.com>
2023-05-17 12:12:22 -0400

    Finished Warm up exercises

--- v.rb
+++ v.rb
@@ -5,6 +5,22 @@
 
   end
 
+  def itera_sum(arr)
+    sum = 0
+    arr.each do |num|
+      sum += num
+    end
+    sum
+  end
 
+  def recur_sum(arr)
+    return [] if arr.empty?
+    return arr[0] if arr.length == 1
+    arr[0] + recur_sum(arr[1..-1])
+  end
+
+  # p itera_sum([1, 2, 3])
+  # p recur_sum([1, 2, 3])
+  # p recur_sum([])
 
 # p  range(1,5)
